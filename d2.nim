51408cc251da27ee54c00c13c9f650775762e0f6
inv2004 <inv2004@gmail.com>
2021-12-02 21:24:35 +0300

    d2

---
+++ d2.nim
@@ -0,0 +1,29 @@
+import strutils
+import sequtils
+
+var n = toSeq(lines("d2.in"))
+  .mapIt(split(it, " "))
+  .mapIt((it[0][0], it[1].parseInt))
+
+var f, d = 0
+
+for x in n:
+  case x[0]
+  of 'f': f += x[1]
+  of 'u': d -= x[1]
+  of 'd': d += x[1]
+  else: assert false
+
+echo f * d
+
+(f, d) = (0, 0)
+var a = 0
+
+for x in n:
+  case x[0]
+  of 'f': f += x[1]; d += a * x[1]
+  of 'u': a -= x[1]
+  of 'd': a += x[1]
+  else: assert false
+
+echo f * d
