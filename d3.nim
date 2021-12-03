bde820585a26ebb04ba3083467c0667ecddab035
inv2004 <inv2004@gmail.com>
2021-12-03 21:40:38 +0300

    d3 upd

--- d3.nim
+++ d3.nim
@@ -10,47 +10,42 @@ func toBit(x: string): uint64 =
     if x == '1':
       result.setBit(h-i)
 
-proc `$$`(x: seq[int]): string =
+converter toStr(x: seq[bool]): string =
   for x in x:
-    result = $x & result
-
-proc flipBit(x: uint64, s: int): uint64 =
-  result = x
-  result.flipMask(uint64(2^s-1))
+    let s = if x: "1" else: "0"
+    result = s & result
 
 var s = toSeq(lines("d3.in"))
 let m = s[0].len
 let n = s.mapIt(fmt"{it:0>64}").map(toBit)
 
-proc calc(n: seq[uint64]): seq[int] =
-  result = newSeq[int](h+1)
+proc calc(n: seq[uint64]): seq[bool] =
+  var r = newSeq[int](h+1)
   for x in n:
     for i in 0..h:
       if x.testBit(i):
-        inc result[i]
+        inc r[i]
       else:
-        dec result[i]
-
-  result.applyIt(it.cmp(-1).max(0))
+        dec r[i]
+  return r.mapIt(it.cmp(-1) > 0)
 
 let r = calc(n)
 
-let p = toBit($$r)
+let p = toBit(r)
+var e = p
+e.flipMask(uint64(2^m-1))
+echo p*e
 
-echo p*flipBit(p, m)
+var ox = n
+var co = n
+for i in countdown(m-1, 0):
+  if ox.len > 1:
+    let b = calc(ox)
+    ox = ox.filterIt(it.testBit(i) == b[i])
+  if co.len > 1:
+    let b = calc(co)
+    co = co.filterIt(it.testBit(i) != b[i])
+  if ox.len == 1 and co.len == 1:
+    break
 
-proc calc2(n: seq[uint64], m: int): (uint64, uint64) =
-  var nn = n
-  var nnn = n
-  for i in countdown(m-1, 0):
-    if nn.len > 1:
-      let b = calc(nn).mapIt(it > 0)
-      nn = nn.filterIt(it.testBit(i) == b[i])
-    if nnn.len > 1:
-      let b = calc(nnn).mapIt(it > 0)
-      nnn = nnn.filterIt(it.testBit(i) != b[i])
-    if nn.len == 1 and nnn.len == 1:
-      return (nn[0], nnn[0])
-
-let (a, b) = calc2(n, m)
-echo a*b
+echo ox[0]*co[0]
