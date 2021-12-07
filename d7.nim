e6bbbde9686c71f43996e5e296efe98782cd2727
alexander <alexander@saporo.io>
2021-12-07 15:54:53 +0300

    d7 part1 and 2

--- d7.nim
+++ d7.nim
@@ -2,4 +2,7 @@ import sequtils, strutils, math, sugar
 
 let n = readFile("d7.in").split(",").map(parseInt)
 
-echo toSeq(0..n.high).map(i => n.map(x => abs(x - i)).map(x => x * (x+1) div 2).sum()).min()
+let a = toSeq(0..n.high).map(i => n.map(x => abs(x - i)))
+
+echo a.mapIt(it.sum()).min()
+echo a.mapIt(it.mapIt(it * (it+1) div 2).sum()).min()
