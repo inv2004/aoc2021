6b92b865ae83518a1a65ac6d587489473bca13cd
alexander <alexander@saporo.io>
2021-12-11 00:57:31 +0300

    d10 v2

--- d10.nim
+++ d10.nim
@@ -1,46 +1,19 @@
-import sequtils, strutils, algorithm
+import sequtils, algorithm, tables
 
-proc pair(c: char): char =
-  case c
-  of ')': return '('
-  of ']': return '['
-  of '}': return '{'
-  of '>': return '<'
-  else: assert false
-
-proc scoreErr(c: char): int =
-  case c
-  of ')': return 3
-  of ']': return 57
-  of '}': return 1197
-  of '>': return 25137
-  else: assert false
-
-proc scoreComp(s: string): int =
-  for c in s:
-    result *= 5
-    case c
-    of '(': result += 1
-    of '[': result += 2
-    of '{': result += 3
-    of '<': result += 4
-    else: assert false
+const pair = {')': '(', ']': '[', '}': '{', '>': '<'}.toTable
+const errScore = {')': 3, ']': 57, '}': 1197, '>': 25137}.toTable
+const compScore = {'(': 1, '[': 2, '{': 3, '<': 4}.toTable
 
 proc check(s: string): (int, int) =
   var r = ""
   for i, x in s:
-    case x
-    of '[', '(', '{', '<':
+    if x in "[({<":
       r.add x
-    of ']', ')', '}', '>':
-      if r[^1] == pair(x):
+    elif r[^1] == pair[x]:
         r.setLen(r.len - 1)
-      else:
-        return (scoreErr x, 0)
     else:
-      assert false
-
-  return (0, scoreComp r.reversed().join())
+      return (errScore[x], 0)
+  return (0, r.reversed().foldl(a*5+compScore[b], 0))
 
 echo toSeq(lines("d10.in")).map(check).mapIt(it[0]).foldr(a+b)
 let r = toSeq(lines("d10.in")).map(check).mapIt(it[1]).filterIt(it > 0).sorted()
