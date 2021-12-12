c265562e6fcbb0455005b3d951b6fa7b545322c0
alexander <alexander@saporo.io>
2021-12-13 00:47:44 +0300

    d12

--- d12.nim
+++ d12.nim
@@ -3,7 +3,6 @@ import sequtils, strutils, tables, itertools, sugar, algorithm
 type T = Table[system.string, seq[TaintedString]]
 
 let pp = toSeq(lines("d12.in")).mapIt(it.split("-")).mapIt(@[(it[0], it[1]), (it[1], it[0])]).foldl(a&b)
-# echo pp
 let p = toSeq(pp.groupBy(x => x[0])).mapIt((it.k, it.v.mapIt(it[1]).sorted())).toTable
 
 proc go(p: T, c: Table[string, int], room = "start"): seq[string] =
@@ -12,21 +11,25 @@ proc go(p: T, c: Table[string, int], room = "start"): seq[string] =
   if room == "end":
     return @[room]
   let rooms = p[room]
-  if cVar[room] > 0:
+  if cVar.contains(room) and cVar[room] > 0:
     dec cVar[room]
   for x in rooms:
-    if cVar[x] == 0:
+    if x == "start":
+      continue
+    if cVar.contains(x) and cVar[x] == 0:
       continue
     for y in go(pVar, cVar, x):
       result.add room & "," & y
 
-proc calc(p: T): seq[string] =
+proc calc(p: T, repeats: int): seq[string] =
   var c = initTable[string, int]()
   for k in p.keys:
-    if k in ["start", "end"]: c[k] = 1
-    elif k in "a".."z": c[k] = 1
-    else: c[k] = -1
-  echo c
-  go(p, c)
-
-echo calc(p).len
+    if k notin ["start", "end"] and k[0] in 'a'..'z':
+      c[k] = 1
+  for i in 0..<c.len:
+    var cc = c
+    cc[toSeq(cc.keys)[i]] = repeats
+    result = deduplicate result & go(p, cc)
+  
+echo calc(p, 1).len
+echo calc(p, 2).len
