cc918510dbdb995fbc5cc723599746047c4eb2a1
inv2004 <inv2004@gmail.com>
2021-12-21 00:49:26 +0300

    d12d

---
+++ d12d.nim
@@ -0,0 +1,47 @@
+import sequtils, strutils, tables, itertools, sugar, algorithm, times, sets
+
+let pp = toSeq(lines("d12c.in")).mapIt(it.split("-")).mapIt(@[(it[0], it[1]), (it[1], it[0])]).foldl(a&b)
+var p = toSeq(pp.groupBy(x => x[0])).filterIt(it.k != "end").mapIt((it.k, (it.v.mapIt(it[1]).filterIt(it != "start").sorted()))).toTable
+
+# op:{m[last x] except x inter s}
+proc f(a: var seq[seq[string]]): bool =
+  for i in 0..a.high:
+    if a[i].len > 0 and a[i][^1] != "end":
+      for xx in p[a[i][^1]]:
+        if not (xx[0] in 'a'..'z' and xx in a[i]):
+          a.add a[i]&xx
+          result = true
+      a[i].setLen 0
+
+# op:{$[2 in s#count each group x;m[last x] except x inter s;m[last x] except `start]}
+proc ff(a: var seq[seq[string]]): bool =
+  for i in 0..a.high:
+    if a[i].len > 0 and a[i][^1] != "end":
+      let s = a[i].toCountTable
+      var c = false
+      for k, v in s:
+        if k[0] in 'a'..'z' and v == 2:
+          c = true
+      for xx in p[a[i][^1]]:
+        if xx[0] in 'a'..'z' and c and xx in s:
+          continue
+        a.add a[i]&xx
+        result = true
+      a[i].setLen 0
+
+var a = @[@["start"]]
+var t = cpuTime()
+while f(a):
+  discard
+a.keepItIf(it.len > 0)
+echo cpuTime()-t
+echo a.len
+
+a = @[@["start"]]
+t = cpuTime()
+while ff(a):
+  discard
+
+a.keepItIf(it.len > 0)
+echo cpuTime()-t
+echo a.len
