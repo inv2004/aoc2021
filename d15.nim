60541d6c9d3ac8c70cc81bd6ff10d91572896a2f
alexander <alexander@saporo.io>
2021-12-28 23:09:47 +0300

    d15: upd

--- d15.nim
+++ d15.nim
@@ -1,7 +1,4 @@
-import sequtils, strutils
-
-type T = seq[seq[int]]
-type TT = (seq[(int, int)], int)
+import sequtils
 
 const mul = 5
 
@@ -18,7 +15,7 @@ proc m(c: seq[seq[int]]): seq[seq[int]] =
             v = v mod 10 + 1
           result[y+(c.len*yy)][x+(c[0].len*xx)] = v
 
-iterator neighbours4(x, y: int): (int, int, int) =
+iterator n4(x, y: int): (int, int, int) =
   for (xx, yy) in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]:
     if xx in 0..c[0].high and yy in 0..c.high:
       yield (xx, yy, c[yy][xx])
@@ -31,7 +28,7 @@ proc bfs(): int =
     let x = q[0][0]
     let y = q[0][1]
     q.delete 0
-    for (nx, ny, nr) in neighbours4(x, y):
+    for (nx, ny, nr) in n4(x, y):
       if cache[ny][nx] > cache[y][x] + nr:
         cache[ny][nx] = cache[y][x] + nr
         q.add((nx, ny))
